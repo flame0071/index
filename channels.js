5aa97f1f05299e1f83ed877ac74d5be90e00332f
flame0071 <jamesbenavides617@gmail.com>
2024-09-21 10:04:59 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,28 +1,13 @@
-// Load channel data from channels.json
-fetch('channels.json')
-    .then(response => response.json())
-    .then(channels => {
-        const channelList = document.getElementById('channelList');
-        channels.forEach(channel => {
-            // Create channel div
-            const channelDiv = document.createElement('div');
-            channelDiv.classList.add('channel');
-            channelDiv.innerHTML = `
-                <img src="${channel.logo}" alt="${channel.name}">
-                <p>${channel.name}</p>
-            `;
-            channelDiv.onclick = () => changeStream(channel.stream);
-
-            // Append to the channel list
-            channelList.appendChild(channelDiv);
-        });
-    });
-
-// Initialize the video player
-const player = videojs('videoPlayer');
-
-// Function to change the video stream
-function changeStream(streamUrl) {
-    player.src({ type: 'application/x-mpegURL', src: streamUrl });
-    player.play();
-}
+const channels = [
+    {
+        name: 'A2Z',
+        src: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558786.m3u8',
+        logo: 'https://static.wikia.nocookie.net/russel/images/8/85/A2Z_Channel_11_without_Channel_11_3D_Logo_2020.png/revision/latest/scale-to-width-down/270?cb=20231101144828'
+    },
+    {
+        name: 'BUKO',
+        src: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558780.m3u8',
+        logo: 'https://static.wikia.nocookie.net/logopedia/images/6/67/BuKo_Channel_(2021-.n.v.).png/revision/latest/scale-to-width-down/200?cb=20210720055409'
+    }
+    // Add more channels here...
+];
