46b02fb96cb22948063a80d5c141105d92d0c870
flame0071 <jamesbenavides617@gmail.com>
2024-09-21 15:00:53 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,13 +1,62 @@
 const channels = [
     {
+        name: 'TV5',
+        logo: 'https://static.wikia.nocookie.net/tv5network/images/b/b9/TV5_logo_2013.png',
+        stream: 'https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/tv5_qp.m3u8'
+    },
+    {
         name: 'A2Z',
-        src: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558786.m3u8',
-        logo: 'https://static.wikia.nocookie.net/russel/images/8/85/A2Z_Channel_11_without_Channel_11_3D_Logo_2020.png/revision/latest/scale-to-width-down/270?cb=20231101144828'
+        logo: 'https://static.wikia.nocookie.net/russel/images/8/85/A2Z_Channel_11_without_Channel_11_3D_Logo_2020.png',
+        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558786.m3u8'
     },
     {
         name: 'BUKO',
-        src: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558780.m3u8',
-        logo: 'https://static.wikia.nocookie.net/logopedia/images/6/67/BuKo_Channel_(2021-.n.v.).png/revision/latest/scale-to-width-down/200?cb=20210720055409'
+        logo: 'https://static.wikia.nocookie.net/logopedia/images/6/67/BuKo_Channel_(2021-.n.v.).png',
+        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558780.m3u8'
+    },
+    {
+        name: 'HITS HD',
+        logo: 'https://www.hitstv.com/assets/hits.png',
+        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558765.m3u8'
+    },
+    {
+        name: 'One News',
+        logo: 'https://static.wikia.nocookie.net/russel/images/5/53/One_News_Logo_2020.png',
+        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558789.m3u8'
+    },
+    {
+        name: 'NHK World',
+        logo: 'https://logotyp.us/file/nhk.svg',
+        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558790.m3u8'
+    },
+    {
+        name: 'Kapamilya',
+        logo: 'https://static.wikia.nocookie.net/philippinetelevision/images/c/c5/Kapamilya_Online_Live_Logo_2020.png/revision/latest/scale-to-width-down/200?cb=20240318102904',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290851.m3u8'
+    },
+    {
+        name: 'AXN',
+        logo: 'https://www.logo.wine/a/logo/AXN_(Portuguese_TV_channel)/AXN_(Portuguese_TV_channel)-Logo.wine.svg',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290856.m3u8'
+    },
+    {
+        name: 'GMA Life TV',
+        logo: 'https://static.wikia.nocookie.net/logopedia/images/7/70/Gma-life-tv-first-logo.jpg/revision/latest/scale-to-width-down/250?cb=20180813090909',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290871.m3u8'
+    },
+    {
+        name: 'GMA News TV',
+        logo: 'https://static.wikia.nocookie.net/logopedia/images/c/cc/GMA_News_TV_2011_logo_animation.png/revision/latest/scale-to-width-down/160?cb=20211211102116',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290872.m3u8'
+    },
+    {
+        name: 'GMA Pinoy TV',
+        logo: 'https://content.osn.com/logo/channel/cropped/GMA.png',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290873.m3u8'
+    },
+    {
+        name: 'Hits Movies',
+        logo: 'https://static.wikia.nocookie.net/logos/images/1/16/HITS_Movies_logo.png/revision/latest/scale-to-width-down/180?cb=20220323123229&path-prefix=vi',
+        stream: 'http://filex.tv:8080/live/gtr/gtr/290875.m3u8'
     }
-    // Add more channels here...
 ];
