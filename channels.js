234887f5017d62d4ed0fddae77604a1f15d621f6
flame0071 <jamesbenavides617@gmail.com>
2024-09-21 09:39:17 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,64 +1,28 @@
-// channel.js
+// Load channel data from channels.json
+fetch('channels.json')
+    .then(response => response.json())
+    .then(channels => {
+        const channelList = document.getElementById('channelList');
+        channels.forEach(channel => {
+            // Create channel div
+            const channelDiv = document.createElement('div');
+            channelDiv.classList.add('channel');
+            channelDiv.innerHTML = `
+                <img src="${channel.logo}" alt="${channel.name}">
+                <p>${channel.name}</p>
+            `;
+            channelDiv.onclick = () => changeStream(channel.stream);
 
-const channels = {
-    'A2Z': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558786.m3u8',
-        logo: 'https://static.wikia.nocookie.net/russel/images/8/85/A2Z_Channel_11_without_Channel_11_3D_Logo_2020.png/revision/latest/scale-to-width-down/270?cb=20231101144828',
-        name: 'A2Z'
-    },
-    'BUKO': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558780.m3u8',
-        logo: 'https://static.wikia.nocookie.net/logopedia/images/6/67/BuKo_Channel_(2021-.n.v.).png/revision/latest/scale-to-width-down/200?cb=20210720055409',
-        name: 'BUKO'
-    },
-    'HITS HD': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558765.m3u8',
-        logo: 'https://www.hitstv.com/assets/hits.png',
-        name: 'HITS HD'
-    },
-    'MPTV': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558764.m3u8',
-        logo: 'http://www.satlitetv.com/wp-content/uploads/2024/05/xMPTV.png.pagespeed.ic.P4eiADsK9O.png',
-        name: 'MPTV'
-    },
-    'Kix': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558763.m3u8',
-        logo: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/KIX_logo.svg',
-        name: 'Kix'
-    },
-    'One News': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558757.m3u8',
-        logo: 'https://static.wikia.nocookie.net/russel/images/5/53/One_News_Logo_2020.png/revision/latest/scale-to-width-down/300?cb=20230428021352',
-        name: 'One News'
-    },
-    'One Sport+': {
-        stream: 'http://starshare.live:8080/live/Pradeep/Pradeep/150981.m3u8',
-        logo: 'https://upload.wikimedia.org/wikipedia/commons/e/ec/OneSportsPlus_logo.svg',
-        name: 'One Sport+'
-    },
-    'PBA RUSH': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558787.m3u8',
-        logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/PBA_Rush_logo.png/220px-PBA_Rush_logo.png',
-        name: 'PBA RUSH'
-    },
-    'Tap Action Flix': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558748.m3u8',
-        logo: 'https://www.lyngsat.com/logo/tv/tt/tap-action-flix-ph.png',
-        name: 'Tap Action Flix'
-    },
-    'Tap Edge': {
-        stream: 'http://apexelite.xyz:3050/live/scottandersonholmes23@gmail.com/e5y43zFzmq/558747.m3u8',
-        logo: 'https://static.wikia.nocookie.net/logopedia/images/c/cd/TAP_Edge-1_logo.png/revision/latest?cb=20210901164013',
-        name: 'Tap Edge'
-    },
-    'Tap Sports': {
-        stream: 'http://starshare.live:8080/live/Pradeep/Pradeep/150969.m3u8',
-        logo: 'https://static.wikia.nocookie.net/russel/images/8/85/TAP_Sports_Logo_2020.png/revision/latest/scale-to-width-down/300?cb=20220402065111',
-        name: 'Tap Sports'
-    },
-    'VIVA Cinema': {
-        stream: 'http://starshare.live:8080/live/Pradeep/Pradeep/151090.m3u8',
-        logo: 'https://static.wikia.nocookie.net/logopedia/images/8/8c/VivaCinemaLogo.png/revision/latest/scale-to-width-down/250?cb=20211206092842',
-        name: 'VIVA Cinema'
-    }
-};
+            // Append to the channel list
+            channelList.appendChild(channelDiv);
+        });
+    });
+
+// Initialize the video player
+const player = videojs('videoPlayer');
+
+// Function to change the video stream
+function changeStream(streamUrl) {
+    player.src({ type: 'application/x-mpegURL', src: streamUrl });
+    player.play();
+}
